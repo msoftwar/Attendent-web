b8a9642c5f89e0ceba29e29b696336742b45e79b
GJAPP <mmatiullah552@gmail.com>
2021-12-13 18:15:15 +0500

    second push updated

--- attendent-web/src/app/app-routing.module.ts
+++ attendent-web/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+const routes: Routes = [
+  {
+    path:'',redirectTo: 'login', pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '', component: AppLayoutComponent, pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'signup',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '', component: SignupComponent, pathMatch: 'full'
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- attendent-web/src/app/app.module.ts
+++ attendent-web/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

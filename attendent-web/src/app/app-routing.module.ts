797917531b3c3af4c536168576c66a4e2772d48d
GJAPP <mmatiullah552@gmail.com>
2021-12-14 21:25:05 +0500

    frontend changes

--- attendent-web/src/app/app-routing.module.ts
+++ attendent-web/src/app/app-routing.module.ts
@@ -1,30 +1,91 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+// import { GoogleMapComponent } from './google-map/google-map.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { MapComponent } from './map/map.component';
 const routes: Routes = [
   {
     path:'',redirectTo: 'login', pathMatch: 'full'
   },
   {
-    path: '',
+    path: 'dashboard',
     component: AppLayoutComponent,
     children: [
       {
-        path: '', component: AppLayoutComponent, pathMatch: 'full'
+        path: '', component: DashboardComponent, pathMatch: 'full'
       }
     ]
   },
   {
     path: 'signup',
-    component: AppLayoutComponent,
+    component: SignupComponent,
     children: [
       {
         path: '', component: SignupComponent, pathMatch: 'full'
       }
     ]
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+    children: [
+      {
+        path: '', component: LoginComponent, pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    children: [
+      {
+        path: '', component: ForgotPasswordComponent, pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'user-profile',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '', component: UserProfileComponent, pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '**', component: PageNotFoundComponent, pathMatch: 'full'
+      }
+    ]
+  }
+ 
+  // {
+  //   path: 'map',
+  //   component: AppLayoutComponent,
+  //   children: [
+  //     {
+  //       path: '', component: MapComponent, pathMatch: 'full'
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: 'google-map',
+  //   component: AppLayoutComponent,
+  //   children: [
+  //     {
+  //       path: '', component: GoogleMapComponent, pathMatch: 'full'
+  //     }
+  //   ]
+  // },
 ];
 
 @NgModule({
--- attendent-web/src/app/app.module.ts
+++ attendent-web/src/app/app.module.ts
@@ -1,23 +1,40 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+// import { GoogleMapComponent } from './google-map/google-map.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
+// import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ForgotPasswordComponent,
     LoginComponent,
     SignupComponent,
-    AppLayoutComponent
+    AppLayoutComponent,
+    UserProfileComponent,
+    DashboardComponent,
+    // // GoogleMapComponent,
+     PageNotFoundComponent,
+    // // MapComponent,
+
   ],
+ 
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule,
+     RouterModule
+   
+   
     
   ],
   providers: [],
